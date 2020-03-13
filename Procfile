f9761241bb7ba2b25071e68514075c2729b86648
Delegant <59837698+Delegant@users.noreply.github.com>
2020-03-14 00:44:27 +0300

    update dependency add proxy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web:    java $JAVA_OPTS -java webapp/target/dependency/ Bot.jar --port $PORT webapp/target/*.war
--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,24 +1,42 @@
 import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.ApiContext;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Bot extends TelegramLongPollingBot {
+
+
+    public Bot(DefaultBotOptions opt){
+        super(opt);
+    }
     public static void main(String[] args) {
-
-        ApiContextInitializer.init();
-
-        TelegramBotsApi botsApi = new TelegramBotsApi();
-
         try {
-            botsApi.registerBot(new Bot());
+        ApiContextInitializer.init();
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+        DefaultBotOptions botOptions = ApiContext.getInstance(DefaultBotOptions.class);
+        botOptions.setBaseUrl("https://telegg.ru/orig/bot");
+        Bot bot = new Bot(botOptions);
+            botsApi.registerBot(bot);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
     }
 
     public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()){
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 
